2880144184d785ff7f5c823d68fea39aa6708080
村雨遥 <cunyu1024@gmail.com>
2026-08-02 16:25:33 +0800

    docs: 初始化计算机学习指南文档站点
    添加了数据结构与算法、计算机组成原理、计算机网络、操作系统四大板块的文档内容，包括各板块的介绍文档、配套图片资源，完善了站点导航与侧边栏配置，更新了首页内容并修正了仓库基础路径配置，同时删除了原有的示例markdown文档。

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -1,12 +1,52 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const sidebar: DefaultTheme.Config['sidebar'] = {
-    
+
     '/': [
-        
+        { text: '学习指引', link: '/guide', },
+
+        {
+            text: '数据结构与算法',
+            collapsed: true,
+            items: [
+                { text: '数据结构简介', link: '/algo/20220503-datastructure-intro' },
+                { text: '数组详解', link: '/algo/20220516-array' },
+                { text: '数组的增删改查', link: '/algo/20200510-arr-crud' },
+                { text: '有序数组去重', link: '/algo/20220513-sorted-arr-duplication' },
+                { text: '算法简介', link: '/algo/20220511-algo-intro' },
+                { text: '时间&空间复杂度', link: '/algo/20220512-time-space' },
+                { text: '链表结果', link: '/algo/20220515-linkedlist' },
+                { text: '二叉树的遍历', link: '/algo/20220517-binary-tree-traverse' },
             
+
+            ]
+        },
+
+        {
+            text: '组成原理',
+            collapsed: true,
+            items: [
                 
-            
-        
+
+            ]
+        },
+
+        {
+            text: '计算机网络',
+            collapsed: true,
+            items: [
+                
+
+            ]
+        },
+
+        {
+            text: '操作系统',
+            collapsed: true,
+            items: [
+                
+
+            ]
+        },
     ],
 }
---
+++ docs/src/guide.md
@@ -0,0 +1,22 @@
+# 计算机学习指南
+
+## 一、简介
+
+## 二、目录
+
+### 2.1 数据结构与算法
+
+1. [数据结构简介](algo/20220503-datastructure-intro)
+2. [数组详解](algo/20220516-array)
+3. [数组的增删改查](algo/20200510-arr-crud)
+4. [有序数组去重](algo/20220513-sorted-arr-duplication)
+5. [算法简介](algo/20220511-algo-intro)
+6. [时间 & 空间复杂度](algo/20220512-time-space)
+7. [链表解惑](algo/20220515-linkedlist)
+8. [二叉树的遍历](algo/20220517-binary-tree-traverse)
+
+### 2.2 组成原理
+
+### 2.3 计算机网络
+
+### 2.4 操作系统
